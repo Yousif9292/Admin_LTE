fd8c4b8fd72d0b5da577b9509a2727d77066a733
Yousif9292 <yousifjalbani@gmail.com>
2023-06-17 03:22:55 +0500

    JS Cropper image

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -56,49 +56,43 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
+        // Validate the form data
         $request->validate([
-        'image' => 'nullable|image',
-        'product_name' => 'required',
-        'purchase_price' => 'required|numeric',
-        'sale_price' => 'required|numeric',
-        'discount' => 'required|numeric',
-        'status' => 'required|boolean',
-        'category_id' => 'required|exists:categories,id',
-    ]);
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:10240',
+            // Add other validation rules for your form fields
+        ]);
 
+        // Store the uploaded image
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $imageName = time() . '.' . $image->getClientOriginalExtension();
 
+            // Crop and resize the image
+            $croppedImage = Image::make($image)->crop(160, 160)->resize(500, 500, function ($constraint) {
+                $constraint->aspectRatio();
+            })->encode();
 
-    // Create a new product instance
-    $product = new Product;
-    $product->product_name = $request->product_name;
-    $product->purchase_price = $request->purchase_price;
-    $product->sale_price = $request->sale_price;
-    $product->discount = $request->discount;
-    $product->status = $request->status;
-    $product->category_id = $request->category_id;
+            // Save the cropped image to the public/images folder
+            // $croppedImage->save(public_path('images/' . $imageName));
 
-    // Handle the image upload if provided
-    if ($request->hasFile('image')) {
-        $image = $request->file('image');
-        $imagePath = time().'.'.$image->getClientOriginalExtension(); // Specify the destination path and file name
-        $destinationPath = public_path('/images');
-        $imgFile = Image::make($image->getRealPath());
-        $imgFile->resize(150, 150, function ($constraint) {
-		    $constraint->aspectRatio();
-		})->save($destinationPath. '/'.$imagePath);
-        $destinationPath = public_path('images');
-       // Move the uploaded image to the specified location
-        $product->image = $imagePath;
+            // Create a new product with the cropped image path
+            $product = new Product();
+            $product->image = 'images/' . $imageName;
+            // Set other product attributes based on your form fields
+            $product->product_name = $request->input('product_name');
+            $product->purchase_price = $request->input('purchase_price');
+            $product->sale_price = $request->input('sale_price');
+            $product->discount = $request->input('discount');
+            $product->status = $request->input('status');
+            $product->category_id = $request->input('category_id');
+            $product->save();
 
+            return redirect()->route('products.index')
+                ->with('success', 'Product created successfully.');
+        }
 
+        return back()->withErrors(['image' => 'Failed to upload image.']);
     }
-
-    // Save the product
-    $product->save();
-
-    // Redirect to the index page with a success message
-    return redirect()->route('products.index')->with('success', 'Product created successfully');
-}
 
     /**
      * Display the specified resource.
